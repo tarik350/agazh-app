10f4e7fe1b43a37e66e68dffd1e38f94f9794895
tarik350 <80385670+tarik350@users.noreply.github.com>
2024-08-21 22:58:19 +0300

    camera support for id card

--- lib/screens/employer_regisration/widgets/personal_info/view/personal_info_form.dart
+++ lib/screens/employer_regisration/widgets/personal_info/view/personal_info_form.dart
@@ -176,16 +176,10 @@ class _ImageUploaderButton extends StatelessWidget {
       builder: (context, state) {
         return GestureDetector(
             onTap: () async {
-              Uint8List image = await pickImage(ImageSource.gallery);
-              if (context.mounted) {
-                context
-                    .read<PersonalInfoBloc>()
-                    .add(IdCardChanged(file: image, path: "idcard"));
-              }
+              _showImageSourceDialog(context);
             },
             child: SizedBox(
               width: double.infinity,
-              // decoration: const BoxDecoration(),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -241,6 +235,45 @@ class _ImageUploaderButton extends StatelessWidget {
             ));
       },
     );
+  }
+
+  void _showImageSourceDialog(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return SafeArea(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.photo_library),
+                    title: const Text('Gallery'),
+                    onTap: () {
+                      _pickImage(context, ImageSource.gallery);
+                      Navigator.of(context).pop();
+                    }),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('Camera'),
+                  onTap: () {
+                    _pickImage(context, ImageSource.camera);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _pickImage(BuildContext context, ImageSource source) async {
+    Uint8List? image = await pickImage(source);
+    if (context.mounted) {
+      context
+          .read<PersonalInfoBloc>()
+          .add(IdCardChanged(file: image!, path: "idcard"));
+    } else {
+      // Handle the case where no image was picked (optional)
+    }
   }
 }
 
--- lib/services/image_service.dart
+++ lib/services/image_service.dart
@@ -14,3 +14,15 @@ pickImage(ImageSource source) async {
 showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
 }
+
+// Future<Uint8List?> pickImage(ImageSource source) async {
+//   final ImagePicker imagePicker = ImagePicker();
+
+//   XFile? file = await imagePicker.pickImage(source: source);
+
+//   if (file != null) {
+//     return await file.readAsBytes();
+//   } else {
+//     return null; // Return null if the user cancels the action
+//   }
+// }
